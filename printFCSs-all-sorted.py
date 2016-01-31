9c1c0136fc512984f8b2e354aadbffec3b933996
Marta Waligórska <marta_h@amu.edu.pl>
2016-01-31 22:15:20 +0100

    Use dictionary to sort by au correctly (in case when there are multiple FCS functions in one file)

--- printFCSs-all-sorted.py
+++ printFCSs-all-sorted.py
@@ -8,11 +8,10 @@ import re # Regular expres.
 #import matplotlib.gridspec as gridspec
 #import matplotlib.pyplot as plt
 #import sFCS_data, getopt 
+import pdb
 
-
-
-def utnij(filename):
-  return filename[4:filename.find("au")]
+def extract_au(filename):
+    return float(re.search('(\d+)au', filename).group(1))
 
 def read_pars_trip(file_now, k):
 	pars = []
@@ -36,7 +35,7 @@ print files
 cmap = plt.get_cmap('jet_r')
 N = len(files)
 if len(files) != 1:
-	files.sort(key = lambda file_name: [float(re.search('(\d+)au', file_name).group(1))])
+	files.sort(key = extract_au)
 
 au_now = -1. 
 c_num = -1.
@@ -50,34 +49,46 @@ for fn in range(len(files)):
 	res_names = l.split()
 	if len(files) == 1:
 		N = len(res_names)
-	#sort by excitation laser power
-	res_names.sort(key = lambda name_now: [float(re.search('(\d+)au', name_now).group(1))])
+		
 	l = f.readline()
 	is_times = l.split('\t')
 	f.close()
 	data = np.loadtxt(inputfile, skiprows=2)
 	FCS_data = data.transpose()
-	for nn in range(0,len(is_times)-1):#
+	for nn in range(0,len(is_times)-1):# -1 bo jest wyeksportowana dodatkowa krzywa w pliku, niepotrzebna
 		if (is_times[nn] == "G(t)[10{^-3}]"):
 			FCS_data[nn] = [y/1000 for y in FCS_data[nn]]
 
 	fig1 = figure(1, figsize=(10,5))
 	fig2 = figure(2, figsize=(10,5))
 
-	aus = [float(re.search('(\d+)au', name_now).group(1)) for name_now in res_names]
-	uW = [au*0.074 for au in aus]
-	for k in range(0,len(res_names)-1): # how many plots from this parameter set
-		if aus[k] != au_now:
-			au_now = aus[k]
-			lab = str("%.2f" % uW[k]) + '$\mu$W'
-			c_num = c_num + 1.
-			color = cmap(c_num/N)
-			fig=figure(1)
-			plt.plot(FCS_data[0], FCS_data[k*3+1], label = lab, c = color)
-			fig=figure(2)
-			plt.plot(FCS_data[0], FCS_data[k*3+1], label = lab, c = color)
-		else:
-			plt.plot(FCS_data[0], FCS_data[k*3+1], c = color)
+	# convert to dictionary
+	dic = {}
+	for i in range(len(res_names) - 1):
+	    name = res_names[i]
+	    fcs = FCS_data[i*3 + 1]
+	    dic[name] = fcs
+	  	
+	# sort by excitation laser power	
+	for filename in sorted(dic, key = extract_au):
+	    au = extract_au(filename)
+	    uW = au*0.074
+	    
+	    fcs = dic[filename]
+
+	    if au != au_now:
+		au_now = au
+		lab = str("%.2f" % uW) + '$\mu$W'
+		c_num = c_num + 1.
+		color = cmap(c_num/N)
+		fig=figure(1)
+		plt.plot(FCS_data[0], fcs, label = lab, c = color)
+		fig=figure(2)
+		plt.plot(FCS_data[0], fcs, label = lab, c = color)
+	    else:
+		plt.plot(FCS_data[0], fcs, c = color)
+
+
 	if len(files) != 1:
 		plt.legend(loc='best')
 		plt.xlim(0.001,100.)
@@ -89,7 +100,7 @@ for fn in range(len(files)):
 		columnspacing=1.0, labelspacing=0.0,
 		handletextpad=0.0, handlelength=1.5,
 		fancybox=True, shadow=True)# bbox_to_anchor=[0.5, 1.1]
-		f_name = 'plots-FCSs/' + re.search('(\d+)au', name_now).group(1) + '.png'
+		f_name = 'plots-FCSs/' + re.search('(\d+)au', files[fn]).group(1) + '.png'
 		print f_name
 		plt.savefig(f_name)
 		plt.clf()
@@ -98,7 +109,17 @@ fig=figure(1)
 plt.legend(loc='best')
 plt.xlim(0.001,100.)
 plt.xscale('log')
-plt.ylim(-0.,5.1)
+
+# Dopasowanie osi Y - WIOCHA!
+def find_index_bigger_than(l, wartosc):
+   for i in xrange(len(l)):
+       if l[i] > wartosc:
+           return i
+
+pierwszy_x = find_index_bigger_than(FCS_data[0], 0.001)
+plt.ylim(-0.,FCS_data[1][pierwszy_x] * 1.2)
+#####
+
 plt.ylabel('FCS')
 plt.xlabel('$\ tau$')
 plt.legend(ncol=3, loc='best', 
@@ -109,30 +130,6 @@ f_name = 'plots-FCSs/all.png'
 print f_name
 plt.savefig(f_name)
 
-#plt.clf()
 
-#for k in range(0,len(names)-1): # how many plots from this parameter set
-	#color = cmap(float(fn)/N)
-	#rho = read_pars_trip(pars_files_trip[fn], k)
-	#lab = str("%.2f" % uW[k]) + '$\mu$W'
-	##textt = textt + names[k][19:-6] + '[au]\n'
-	##print textt
-	#FCS_n = [y/rho for y in FCS_data[k*3+1]]
-	#if k == len(names)-2:
-		#plt.plot(FCS_data[0], FCS_n, label = lab, c = color)
-	#else:
-		#plt.plot(FCS_data[0], FCS_n, c = color)
-		## Inset
-#plt.legend(loc='best')
-#plt.xlim(0.001,100.)
-#plt.xscale('log')
-#plt.ylim(-0.,1.05)
-#plt.ylabel('FCS, norm')
-#plt.xlabel('$\ tau$')
-#plt.legend(ncol=3, loc='best', 
-#columnspacing=1.0, labelspacing=0.0,
-#handletextpad=0.0, handlelength=1.5,
-#fancybox=True, shadow=True)# bbox_to_anchor=[0.5, 1.1]
-#f_name = 'plots-FCSs/all-norm.png'
-#plt.savefig(f_name)
-	
+
+ 
